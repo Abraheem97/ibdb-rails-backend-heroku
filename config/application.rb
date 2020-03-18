05987b76b32a88deea95d6b3f2f508305e03fac2
Abraheem97 <abraheem.asad@devsinc.com>
2020-03-18 19:43:37 +0500

    Changed default action mailer settings and cors set to '*'

--- config/application.rb
+++ config/application.rb
@@ -18,7 +18,7 @@ module Ibdb
 
     Rails.application.config.middleware.insert_before 0, Rack::Cors do 
       allow do
-        origins ['http://localhost:3000']
+        origins '*'
       
         resource '*',
           headers: :any,
