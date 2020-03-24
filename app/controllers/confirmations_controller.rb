97d81614117c625a765da60359555686b9563fa0
Abraheem97 <abraheem.asad@devsinc.com>
2020-03-24 12:14:20 +0500

    Changed the after confirmation path and default url of confirmation from http to https

--- app/controllers/confirmations_controller.rb
+++ app/controllers/confirmations_controller.rb
@@ -3,7 +3,7 @@ class ConfirmationsController < Devise::ConfirmationsController
   private
 
   def after_confirmation_path_for(resource_name, resource)
-    'http://localhost:3000/login'
+    'https://ibdb-react-frontend.herokuapp.com/login'
   end
 
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,7 +66,7 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
   config.action_mailer.delivery_method = :smtp
     
-  config.action_mailer.default_url_options = { host: 'ibdb-rails-backend.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'https://ibdb-rails-backend.herokuapp.com' }
 
   config.action_mailer.smtp_settings = {
     user_name: "noreplyibdb@gmail.com",
