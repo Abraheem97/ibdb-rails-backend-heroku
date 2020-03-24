3fc9d9c3dd3e07a758076a4320e69a40b55e2519
Abraheem97 <abraheem.asad@devsinc.com>
2020-03-24 16:09:54 +0500

    image 2

--- app/models/book.rb
+++ app/models/book.rb
@@ -3,7 +3,7 @@ class Book < ApplicationRecord
 
   scope :by_both, -> (search) { where("author_name LIKE ? OR title LIKE ?", "%#{search}%", "%#{search}%") }
   after_create :set_image_path
-  after_update :set_image_path
+
    
   has_attached_file :image, styles: { medium: '250x300>' }, :storage => :cloudinary,:path => ':id/:style/:filename'
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
@@ -13,7 +13,9 @@ class Book < ApplicationRecord
 
   def set_image_path
     self.image_url = self.image.url
-    self.save
+  self.save
+    
+    
   end
 
 end
