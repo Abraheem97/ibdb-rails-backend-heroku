7537ad6958906776ccac3238a64845ae320678b1
Abraheem97 <abraheem.asad@devsinc.com>
2020-03-27 15:01:38 +0500

    removed default avatar saving from backend

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,9 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable, :confirmable
   has_many :reviews, dependent: :destroy
   has_many :comments, dependent: :destroy
-  has_attached_file :avatar, styles: { medium: '100x100>', thumb: '59x59>' },
-                             default_url: 'https://res.cloudinary.com/dbqes9wsk/image/upload/v1585050544/defaults/missing_lmgsmf.png', storage: :cloudinary, path: ':id/:style/:filename'
+  has_attached_file :avatar, styles: { medium: '100x100>', thumb: '59x59>' }, storage: :cloudinary, path: ':id/:style/:filename'
   validates_attachment_content_type :avatar, content_type: %r{\Aimage/.*\z}
-  after_create :set_image_path
+ 
 
   def username
     email.split('@').first
@@ -17,12 +16,7 @@ class User < ApplicationRecord
     Thread.current[:user]
   end
 
-  def set_image_path
-    unless image_url
-      self.image_url = avatar.url
-      self.save
-    end
-  end
+
 
   def self.current=(user)
     Thread.current[:user] = user
