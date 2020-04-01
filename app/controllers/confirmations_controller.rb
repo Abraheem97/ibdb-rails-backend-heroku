f0b0a567f76b3d4a134d3c553fde4bd2ad482553
Abraheem97 <abraheem.asad@devsinc.com>
2020-04-01 18:16:46 +0500

    Now after confirming email redirected to production app instead of staging app

--- app/controllers/confirmations_controller.rb
+++ app/controllers/confirmations_controller.rb
@@ -2,6 +2,6 @@ class ConfirmationsController < Devise::ConfirmationsController
   private
 
   def after_confirmation_path_for(_resource_name, _resource)
-    'https://ibdb-react-frontend.herokuapp.com/login'
+    'https://ibdb-production.herokuapp.com/login'
   end
 end
